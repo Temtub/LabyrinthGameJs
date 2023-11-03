9d4e82ba0f453fadb940248a7f9d578af2346cd4
rodri <rodromcer04@hotmail.com>
2023-11-03 21:00:35 +0100

    Player creation and starting player movement

--- js/scriptGame.js
+++ js/scriptGame.js
@@ -1,4 +1,3 @@
-import "./gameFunctions"
 
 const boardGrid = document.getElementById("board")
 const boardSize = 64
@@ -10,18 +9,110 @@ const boardArray = [
     [4, 0, 1, 0, 1, 0, 1, 1],
     [1, 0, 0, 0, 1, 0, 0, 1],
     [1, 1, 1, 0, 1, 1, 0, 1],
-    [1, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 3, 0 , 0, 1, 0, 1],
     [1, 1, 0, 1, 1, 1, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 3, 1]
-  ];
+    [1, 1, 1, 1, 1, 1, 0, 1]
+]
 
-//Flujo del juego
+//Variable para guardar la posicion del jugador
+let playerPosition
+
+//Functions of the game
+/**
+ * Funcion que carga las fichas del tablero que son suelos formado por divs y grids
+ */
+const cargarSuelo = () =>{
+
+  let fragment = document.createDocumentFragment()
+
+  for(let i=0; i< boardSize; i++){
+
+      let floor=document.createElement("DIV")
+      floor.classList.add("floorTile")
+      floor.id = i
+      
+      fragment.append(floor)
+
+  }
+
+  boardGrid.append(fragment)
+}
+
+const cargarParedes = () =>{
+  //Array que contiene todas las fichas en un array
+  let arrayFichas = boardGrid.getElementsByTagName("DIV")
+  let contadorFichas=0
+
+  //Recorremos el array
+  for(let i = 0; i<boardArray.length; i++){
+
+      for(let j=0; j<boardArray.length; j++){
+
+          //Comprobamos si es un muro
+          if(boardArray[i][j]== 1){
+                  //Si es un muro cambiamos el aspecto de pared a muro
+                  arrayFichas[contadorFichas].classList.remove("floorTile")
+                  arrayFichas[contadorFichas].classList.add("wallTile")
+
+              }
+              contadorFichas++
+          }
+  }
+
+}
+
+
+/**
+* Funcion para cargar la linea de meta y la linea de salida
+*/
+const cargarMetaInicioJugador = () =>{
+  let arrayFichas = boardGrid.getElementsByTagName("DIV")
+  let contadorFichas=0
+
+  for(let i = 0; i<boardArray.length; i++){
+
+      for(let j=0; j<boardArray.length; j++){
+
+          if(boardArray[i][j] == 4){
+              arrayFichas[contadorFichas].classList.remove("floorTile")
+              arrayFichas[contadorFichas].classList.add("endTile")
+          }
+          if(boardArray[i][j] == 3){
+              arrayFichas[contadorFichas].classList.remove("floorTile")
+              arrayFichas[contadorFichas].classList.add("endTile")
+
+              //Creacion del jugador y colocación en su ficha inicial
+              let character = document.createElement("DIV")
+              //Le damos sus estilos
+              character.classList.add("character")
+              //Guardamos la posicion del jugador
+              playerPosition = boardArray[i][j]
+              //Lo incluimos en su ficha
+              arrayFichas[contadorFichas].append(character)
+              
+          }
+          contadorFichas++
+      }
+  }
+}
+
+
+//FUNCIONES DEL JUGADOR
+
+const moveCharacter =(event) =>{
+  console.log(event)
+  let key = event.target
+
+  if(key == "ArrowRight")
+  
+}
+//Creacion del tablero
 document.addEventListener("DOMContentLoaded", () =>{
     cargarSuelo()
     cargarParedes()
-    cargarMetaInicio()
+    cargarMetaInicioJugador()
 })
-////////////////////////////////////////////////////////////////////////////////////////////////
-//PREGUNTAS: se puede hacer un find a un array bidimensional?, como unir varios ficheros JS?
-////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Movimiento del jugador
+document.addEventListener("keydown", moveCharacter)
