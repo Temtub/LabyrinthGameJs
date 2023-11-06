939b0d3bac6ba6a64f33c0982fed601550c95ca8
Temptub <rodromcer04@hotmail.com>
2023-11-06 11:29:08 +0100

    Character movement rigth

--- js/scriptGame.js
+++ js/scriptGame.js
@@ -9,56 +9,66 @@ const boardArray = [
     [4, 0, 1, 0, 1, 0, 1, 1],
     [1, 0, 0, 0, 1, 0, 0, 1],
     [1, 1, 1, 0, 1, 1, 0, 1],
-    [1, 0, 3, 0 , 0, 1, 0, 1],
+    [1, 0, 3, 0, 0, 1, 0, 1],
     [1, 1, 0, 1, 1, 1, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 1],
     [1, 1, 1, 1, 1, 1, 0, 1]
 ]
 
-//Variable para guardar la posicion del jugador
-let playerPosition
+//Var to save the tiles of the board
+let arrayFichas = boardGrid.getElementsByTagName("DIV")
+
+//Variable to save the position of the player
+let playerPositionI
+let playerPositionJ
+let tileId
+
+//Variable to save the size of the board
+const boardWidth = boardArray[0].length
+const boardHeight = boardArray.length;
+
 
 //Functions of the game
 /**
  * Funcion que carga las fichas del tablero que son suelos formado por divs y grids
  */
-const cargarSuelo = () =>{
+const loadFloor = () => {
 
-  let fragment = document.createDocumentFragment()
+    let fragment = document.createDocumentFragment()
 
-  for(let i=0; i< boardSize; i++){
+    for (let i = 0; i < boardSize; i++) {
 
-      let floor=document.createElement("DIV")
-      floor.classList.add("floorTile")
-      floor.id = i
-      
-      fragment.append(floor)
+        let floor = document.createElement("DIV")
+        floor.classList.add("floorTile")
+        floor.id = i
 
-  }
+        fragment.append(floor)
 
-  boardGrid.append(fragment)
+    }
+
+    boardGrid.append(fragment)
 }
 
-const cargarParedes = () =>{
-  //Array que contiene todas las fichas en un array
-  let arrayFichas = boardGrid.getElementsByTagName("DIV")
-  let contadorFichas=0
+const loadWalls = () => {
+    //Array que contiene todas las fichas en un array
+    let arrayFichas = boardGrid.getElementsByTagName("DIV")
+    let contadorFichas = 0
 
-  //Recorremos el array
-  for(let i = 0; i<boardArray.length; i++){
+    //Recorremos el array
+    for (let i = 0; i < boardArray.length; i++) {
 
-      for(let j=0; j<boardArray.length; j++){
+        for (let j = 0; j < boardArray.length; j++) {
 
-          //Comprobamos si es un muro
-          if(boardArray[i][j]== 1){
-                  //Si es un muro cambiamos el aspecto de pared a muro
-                  arrayFichas[contadorFichas].classList.remove("floorTile")
-                  arrayFichas[contadorFichas].classList.add("wallTile")
+            //Comprobamos si es un muro
+            if (boardArray[i][j] == 1) {
+                //Si es un muro cambiamos el aspecto de pared a muro
+                arrayFichas[contadorFichas].classList.remove("floorTile")
+                arrayFichas[contadorFichas].classList.add("wallTile")
 
-              }
-              contadorFichas++
-          }
-  }
+            }
+            contadorFichas++
+        }
+    }
 
 }
 
@@ -66,53 +76,141 @@ const cargarParedes = () =>{
 /**
 * Funcion para cargar la linea de meta y la linea de salida
 */
-const cargarMetaInicioJugador = () =>{
-  let arrayFichas = boardGrid.getElementsByTagName("DIV")
-  let contadorFichas=0
+const loadEndStarPlayer = () => {
+    
+    let contadorFichas = 0
 
-  for(let i = 0; i<boardArray.length; i++){
+    for (let i = 0; i < boardArray.length; i++) {
 
-      for(let j=0; j<boardArray.length; j++){
+        for (let j = 0; j < boardArray.length; j++) {
 
-          if(boardArray[i][j] == 4){
-              arrayFichas[contadorFichas].classList.remove("floorTile")
-              arrayFichas[contadorFichas].classList.add("endTile")
-          }
-          if(boardArray[i][j] == 3){
-              arrayFichas[contadorFichas].classList.remove("floorTile")
-              arrayFichas[contadorFichas].classList.add("endTile")
+            if (boardArray[i][j] == 4) {
+                arrayFichas[contadorFichas].classList.remove("floorTile")
+                arrayFichas[contadorFichas].classList.add("endTile")
+            }
+            if (boardArray[i][j] == 3) {
+                arrayFichas[contadorFichas].classList.remove("floorTile")
+                arrayFichas[contadorFichas].classList.add("endTile")
 
-              //Creacion del jugador y colocación en su ficha inicial
-              let character = document.createElement("DIV")
-              //Le damos sus estilos
-              character.classList.add("character")
-              //Guardamos la posicion del jugador
-              playerPosition = boardArray[i][j]
-              //Lo incluimos en su ficha
-              arrayFichas[contadorFichas].append(character)
-              
-          }
-          contadorFichas++
-      }
-  }
+                //We save the position of the player
+                playerPositionI = i
+                playerPositionJ = j
+                tileId = contadorFichas
+
+                createPlayer(tileId)
+            }
+            contadorFichas++
+        }
+    }
 }
 
 
 //FUNCIONES DEL JUGADOR
-
-const moveCharacter =(event) =>{
-  console.log(event)
-  let key = event.target
-
-  if(key == "ArrowRight")
+/**
+ * Function to move detect where the player is moving
+ * 
+ * @param {*} event 
+ */
+const moveCharacter = (event) => {
   
+    //We declare the variable of the key that have been pushed
+    let keyPushed = event.key
+    
+    //If player pushes rigth arrow or letter D it moves rigth
+    if (keyPushed == "ArrowRight" || keyPushed == "d") {
+
+        //We search if the player can move to that tile
+        if (canPlayerMoveRigth()) {
+            moveCharacterRight()
+
+        }
+        else{
+            console.log("no se puede m,over")
+
+        }
+    }
+
 }
-//Creacion del tablero
-document.addEventListener("DOMContentLoaded", () =>{
-    cargarSuelo()
-    cargarParedes()
-    cargarMetaInicioJugador()
+
+/**
+ * Function to check if the right tile is a wall or not
+ * 
+ * @returns true || false
+ */
+const canPlayerMoveRigth = () => {
+    
+    //Check if the rigth tile is a wall or not and checks if its the end of the maze
+    if (boardArray[playerPositionI][playerPositionJ+1] === 1  ) {
+        console.log(playerPositionI)
+        console.log(playerPositionJ)
+
+        return false
+    }
+    if (playerPositionJ+1>=boardWidth) {
+        return false
+    }
+    else {
+        return true
+    }
+}
+
+/**
+ * Function that moves the player
+ * 
+ */
+const moveCharacterRight = () => {
+    //We sum one to the position of the player
+    playerPositionJ++
+    //We delete the character from his actual tile
+    deleteCharacter()
+
+    //We sum one to the tile id
+    tileId++
+
+    createPlayer(tileId)
+
+}
+
+const deleteCharacter = () =>{
+    //Search the childs of the tile that the player is in 
+    let childsToDelete = arrayFichas[tileId].getElementsByTagName("DIV")
+
+    //We delete it from there
+    arrayFichas[tileId].removeChild(childsToDelete[0])
+}
+
+/**
+ * Function to create the character in the visible board 
+ * 
+ * @param {int} tileId //Tile where the character is going to be created
+ */
+const createPlayer = (tileId) =>{
+    //Creation of the player 
+    let character = document.createElement("DIV")
+
+    //Give the style to the character
+    character.classList.add("character")
+
+    //We add the player to his tile
+    arrayFichas[tileId].append(character)
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//FLUJO DEL JUEGO
+//Creation of the 
+document.addEventListener("DOMContentLoaded", () => {
+    loadFloor()
+    loadWalls()
+    loadEndStarPlayer()
 })
 
 //Movimiento del jugador
-document.addEventListener("keydown", moveCharacter)
+document.addEventListener("keydown", (event) => {
+
+    //We make the action of the movement
+    console.log(playerPositionI)
+    console.log(playerPositionJ)
+    
+    moveCharacter(event)
+
+})
